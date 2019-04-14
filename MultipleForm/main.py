29ae1c733f9d538049fd6502bde3f871216de986
tegar iman <44028234+tegar12@users.noreply.github.com>
2019-04-14 16:47:40 +0700

    Create main.py

---
+++ MultipleForm/main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from MainForm import *
+
+if __name__=='__main__':
+        a = QApplication(sys.argv)
+
+        minform = MainForm()
+        minform.show()
+        a.exec_()
+
+    © 2019 GitHub, Inc.
+    Terms
+    Privacy
+    Sec
